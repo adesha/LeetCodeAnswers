bcce95c9587d4b323cfb3c27a9bca02f6435bfc0
adesha <54369188+adesha@users.noreply.github.com>
2022-06-16 12:11:29 +0530

    Time: 12 ms (18.91%), Space: 9.6 MB (39.84%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
@@ -3,7 +3,7 @@ public:
     bool isAlienSorted(vector<string>& words, string order) {
         unordered_map<char, char> umap;
         for(int i=0; i<26; i++) {
-            umap[order[i]] = 'a'+i; // h -> a, l -> b
+            umap[order[i]] = 'a'+i; 
         }
         
         for(auto& word : words) {
